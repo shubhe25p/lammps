4a52c607b311939d75a87e1c5893d56e3d28aff7
rgayatri <rgayatri@lbl.gov>
2020-05-14 15:03:29 -0700

    Added lammps dir back.

---
+++ lammps/cmake/Modules/Packages/USER-COLVARS.cmake
@@ -0,0 +1,27 @@
+if(PKG_USER-COLVARS)
+
+  set(COLVARS_SOURCE_DIR ${LAMMPS_LIB_SOURCE_DIR}/colvars)
+
+  file(GLOB COLVARS_SOURCES ${COLVARS_SOURCE_DIR}/[^.]*.cpp)
+
+  # Build Lepton by default
+  option(COLVARS_LEPTON "Build and link the Lepton library" ON)
+
+  if(COLVARS_LEPTON)
+    set(LEPTON_DIR ${LAMMPS_LIB_SOURCE_DIR}/colvars/lepton)
+    file(GLOB LEPTON_SOURCES ${LEPTON_DIR}/src/[^.]*.cpp)
+    add_library(lepton STATIC ${LEPTON_SOURCES})
+    target_include_directories(lepton PRIVATE ${LEPTON_DIR}/include)
+  endif()
+
+  add_library(colvars STATIC ${COLVARS_SOURCES})
+  target_include_directories(colvars PUBLIC ${LAMMPS_LIB_SOURCE_DIR}/colvars)
+  list(APPEND LAMMPS_LINK_LIBS colvars)
+
+  if(COLVARS_LEPTON)
+    list(APPEND LAMMPS_LINK_LIBS lepton)
+    target_compile_options(colvars PRIVATE -DLEPTON)
+    target_include_directories(colvars PUBLIC ${LEPTON_DIR}/include)
+  endif()
+
+endif()
