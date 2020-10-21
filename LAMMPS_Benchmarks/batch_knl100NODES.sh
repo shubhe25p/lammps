952a06a5ffe58bb255f3d1a2ebf17122a6de3b92
rgayatri <rgayatri@lbl.gov>
2020-10-21 16:12:33 -0700

    Updated build script for gpu build. Added a job script for 100 knl node job.

---
+++ LAMMPS_Benchmarks/batch_knl100NODES.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+#SBATCH --nodes=10
+#SBATCH -C knl
+#SBATCH -S 4
+#SBATCH --time=00:30:00
+#SBATCH --qos=debug
+
+NODES=100
+MPIPNODE=64
+THREADS=4
+EXE=lmp_knl
+
+echo "==================================================="
+echo "Running a SNAP benchmark on ${NODES} KNL nodes."
+echo "Each KNL node uses ${MPINODE} MPI ranks
+      and each MPI rank uses ${threads} number of threads."
+echo"      2J14 problem size"
+echo "==================================================="
+
+export OMP_PLACES=threads
+export OMP_PROC_BIND=true
+export OMP_NUM_THREADS=${THREADS}
+
+srun -u --cpu_bind=cores --nodes ${NODES} --ntasks-per-node ${MPIPNODE} ./${EXE} -k on t ${THREADS} -sf kk -in in.snap.test -var nsteps 10 -var nx 10 -var ny 10 -var nz 10 -var snapdir 2J14_W.SNAP/
--- build_lammps_V100.sh
+++ build_lammps_V100.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
 set -xe
-export CRAYPE_LINK_TYPE=dynamic
 
-# Need a Kokkos DOWNLOAD to set the path to nvcc_wrapper.
-KOKKOS_PATH=$HOME/Kokkos/kokkos
+module purge
+module load esslurm
+module load cuda/10.2.89
+module load gcc/8.3.0
+module load openmpi/4.0.2
 
 # Clone the lammps github repo if not already cloned.
 if [ ! -d lammps ]; then
@@ -27,11 +29,17 @@ fi
 cd build_V100
 
 # CMake build statement
-cmake -D CMAKE_INSTALL_PREFIX=$PWD/../install_V100/ \
-  -D CMAKE_CXX_COMPILER=$KOKKOS_PATH/bin/nvcc_wrapper \
-  -D PKG_KOKKOS=ON -D DOWNLOAD_KOKKOS=ON -DKokkos_ENABLE_CUDA=ON -D Kokkos_ARCH_VOLTA70=ON \
-  -D CMAKE_CXX_FLAGS="--expt-extended-lambda" \
-  -D PKG_SNAP=ON \
+cmake \
+  -D CMAKE_INSTALL_PREFIX=$PWD/../install_V100 \
+  -D Kokkos_ARCH_VOLTA70=ON \
+  -D CMAKE_BUILD_TYPE=Release \
+  -D MPI_CXX_COMPILER=mpicxx \
+  -D BUILD_MPI=yes \
+  -D CMAKE_CXX_COMPILER=$PWD/../lib/kokkos/bin/nvcc_wrapper \
+  -D PKG_SNAP=yes \
+  -D PKG_GPU=no \
+  -D PKG_KOKKOS=yes \
+  -D Kokkos_ENABLE_CUDA=yes \
   ../cmake
 
 # make && make install
