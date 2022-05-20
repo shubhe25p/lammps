5f17893a077e671ca0996f05a4e913c4854744ee
Rahulkumar Gayatri <rgayatri@lbl.gov>
2022-05-20 09:12:19 -0700

    Adding sample outputs for tiny,small,medium and large problem sizes and the associated job scripts.

--- benchmarks/3_small/run_small_A100.sh
+++ benchmarks/3_small/run_small_A100.sh
@@ -1,23 +1,23 @@
 #!/bin/bash -l
 #SBATCH -C gpu
 #SBATCH -t 00:10:00
-#SBATCH -J lmp_micro
-#SBATCH -o lmp_micro.o%j
+#SBATCH -J lmp_small
+#SBATCH -o lmp_small.o%j
 #SBATCH -A nstaff_g
-#SBATCH -n 1
+#SBATCH -n 4
 #SBATCH -c 32
 #SBATCH --gpus-per-task=1
 #SBATCH --gpu-bind=none
 
 # spec.txt provides the input specification
 # by defining the variables spec and BENCH_SPEC
-source micro_spec.txt
+source small_spec.txt
 
 mkdir lammps_$spec.$SLURM_JOB_ID
 cd    lammps_$spec.$SLURM_JOB_ID
 ln -s ../../common .
 cp ${0} .
-cp ../micro_spec.txt .
+cp ../small_spec.txt .
 
 # This is needed if LAMMPS is built using cmake.
 export LD_LIBRARY_PATH=../../../perlmutter/build_pm/lib64:$LD_LIBRARY_PATH
@@ -28,7 +28,7 @@ module load PrgEnv-gnu
 module load cudatoolkit
 module load craype-accel-nvidia80
 
-gpus_per_node=1
+gpus_per_node=4
 
 input="-k on g $gpus_per_node -sf kk -pk kokkos newton on neigh half ${BENCH_SPEC} " 
 
--- benchmarks/4_medium/run_medium_A100.sh
+++ benchmarks/4_medium/run_medium_A100.sh
@@ -1,23 +1,23 @@
 #!/bin/bash -l
 #SBATCH -C gpu
-#SBATCH -t 00:10:00
-#SBATCH -J lmp_micro
-#SBATCH -o lmp_micro.o%j
+#SBATCH -t 00:30:00
+#SBATCH -J lmp_medium
+#SBATCH -o lmp_medium.o%j
 #SBATCH -A nstaff_g
-#SBATCH -n 1
+#SBATCH -n 32
 #SBATCH -c 32
 #SBATCH --gpus-per-task=1
 #SBATCH --gpu-bind=none
 
 # spec.txt provides the input specification
 # by defining the variables spec and BENCH_SPEC
-source micro_spec.txt
+source medium_spec.txt
 
 mkdir lammps_$spec.$SLURM_JOB_ID
 cd    lammps_$spec.$SLURM_JOB_ID
 ln -s ../../common .
 cp ${0} .
-cp ../micro_spec.txt .
+cp ../medium_spec.txt .
 
 # This is needed if LAMMPS is built using cmake.
 export LD_LIBRARY_PATH=../../../perlmutter/build_pm/lib64:$LD_LIBRARY_PATH
@@ -27,8 +27,9 @@ EXE=../../../perlmutter/build_pm/bin/lmp
 module load PrgEnv-gnu
 module load cudatoolkit
 module load craype-accel-nvidia80
+export MPICH_GPU_SUPPORT_ENABLED=1
 
-gpus_per_node=1
+gpus_per_node=4
 
 input="-k on g $gpus_per_node -sf kk -pk kokkos newton on neigh half ${BENCH_SPEC} " 
 
